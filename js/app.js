3631eb59d0b5e045246f687db16ea142dacc0124
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2018-07-15 22:06:19 +0200

    Created: array of cards, vue instance and card element <li>

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const cards = [
+  {
+    front: "cat",
+    back: "kot",
+    flipped: false
+  },
+  {
+    front: "snake",
+    back: "wąż",
+    flipped: false
+  },
+  {
+    front: "ladybird",
+    back: "biedronka",
+    fipped: false
+  },
+  {
+    front: "rabbit",
+    back: "królik",
+    flipped: false
+  },
+  {
+    front: "hamster",
+    back: "chomik",
+    flipped: false
+  },
+  {
+    front: "spider",
+    back: "pająk",
+    flipped: false
+  },
+  {
+    front: "turtle",
+    back: "żółw",
+    flipped: false
+  }
+];
+
+new Vue ({
+  el: "#app",
+  data: {
+    cards: cards
+  },
+  methods: {
+
+  }
+});
